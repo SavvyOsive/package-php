17415cf12813a842bd66fe3748f6230dfe1a013f
SavvyOsive <savvy.osive@gmail.com>
2021-07-15 03:09:11 +0530

    Added conversions

---
+++ src/Weight.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Savvyosive\UnitConversions;
+
+class Weight
+{
+    public static function fromkilograms(float $kilograms):self
+    {
+        return new static($kilograms);
+    }
+
+    public function __construct(
+        public float $kilograms
+    ) {
+    }
+
+    public function toLbs(): float
+    {
+        return $this->kilograms * 2.2046;
+    }
+}
---
+++ tests/WeightTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Savvyosive\UnitConversions\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Savvyosive\UnitConversions\Weight;
+
+
+class WeightTest extends TestCase
+{
+    /** @test */
+    public function it_can_convert_kilograms_lbs()
+    {
+        $lbs = Weight::fromkilograms(100)->toLbs();
+        //$lbs = (new Weight(56))->toLbs();
+
+        $this->assertEquals(220.46, $lbs);
+    }
+}
